011be922e864d29736205e0e28d679ccb6c2cad8
ju315 <pbju92@gmail.com>
2024-12-23 22:37:37 +0900

    fix: repository 적용 오류 수정2

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,13 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Board } from './board.entity';
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
 
-@EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+import { Board } from './board.entity';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatus } from './board-status.enum';
+
+@Injectable()
+export class BoardRepository extends Repository<Board> {
+  constructor(dataSource: DataSource) {
+    super(Board, dataSource.createEntityManager());
+  }
+}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   controllers: [BoardsController],
-  providers: [BoardsService],
+  providers: [BoardsService, BoardRepository],
 })
 export class BoardsModule {}
