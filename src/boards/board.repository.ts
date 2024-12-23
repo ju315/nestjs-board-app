beaec61dd2bd4b5ef8b7f40646fe4792566f5b4a
ju315 <pbju92@gmail.com>
2024-12-23 22:15:19 +0900

    fix: repository 적용 오류 수정

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './board.entity';
 
-@EntityRepository()
+@EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {}
