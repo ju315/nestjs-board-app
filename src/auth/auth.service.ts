9278ea62b9f6bf2b7ab43e952a6c9ffadfce5b2a
ju315 <pbju92@gmail.com>
2025-01-02 21:19:38 +0900

    add: POST signup 기능 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { UserRepository } from './user.repository';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,8 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  constructor(dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}
