2d00047bedc31ca085e9daf32f1a9ff8c4ec7019
ju315 <pbju92@gmail.com>
2025-01-02 21:51:49 +0900

    add: password 암호화 저장 로직 추가

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -4,6 +4,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
@@ -16,12 +17,15 @@ export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
     } catch (err) {
-      console.log(err);
       if (err.code === '23505') {
         // 중복 발생 에러 코드 값은 23505이다.
         throw new ConflictException('Existing username.');
