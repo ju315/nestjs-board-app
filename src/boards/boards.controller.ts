2d3bc3227e23560677e0005fbd01289e3e1b92f7
ju315 <pbju92@gmail.com>
2025-01-07 21:39:33 +0900

    add: logger

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   ParseIntPipe,
   Patch,
@@ -24,10 +25,14 @@ import { User } from 'src/auth/user.entity';
 @Controller('boards')
 @UseGuards(AuthGuard())
 export class BoardsController {
+  private logger = new Logger(BoardsController.name);
+
   constructor(private boardService: BoardsService) {}
 
   @Get('/')
   getAllBoard(@GetUser() user: User): Promise<Board[]> {
+    this.logger.verbose(`User ${user.username} trying to get all boards.`);
+
     return this.boardService.getAllBoards(user);
   }
 
@@ -37,6 +42,9 @@ export class BoardsController {
     @Body() createBoardDto: CreateBoardDto,
     @GetUser() user: User,
   ): Promise<Board> {
+    this.logger.verbose(
+      `User ${user.username} creating a new boards. (Payload: ${JSON.stringify(createBoardDto)})`,
+    );
     return this.boardService.createBoard(createBoardDto, user);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+
+  await app.listen(port);
+
+  Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
